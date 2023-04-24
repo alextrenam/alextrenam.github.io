3baaecb34c8830dce2a254afad061fa76db5420c
alextrenam <alextrenam@gmail.com>
2023-04-24 20:20:15 +0100

    Changed Papers tto Research outputs

--- src/components/Outputs.js
+++ src/components/Outputs.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
-class Papers extends Component {
+class Outputs extends Component {
 	render() {
             return (
 		<div class="flex-column">
-		    <h3>Papers</h3>
+		    <h3>Research outputs</h3>
 		    <h4>Coming soon...</h4>
 		</div>
 	    );
   	}
 }
 
-export default Papers;
+export default Outputs;
